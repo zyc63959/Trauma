ac47e5613f6db5bca0766fec677c0eadcd82a9ba
Ransomware Evil <126243832+NiREvil@users.noreply.github.com>
2024-05-21 23:55:57 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 // src/worker.js
 import { connect } from "cloudflare:sockets";
 let password = 'auto';
-let proxyIP = 'usa.revil.link';// 'ni.radically.pro';
+let proxyIP = '';// ['usa.revil.link', 'ni.radically.pro'];
 
 let addresses = [
 	// everything you want, When sub is empty, the local preferred domain name/preferred IP is enabled. If the port number is not provided, the default TLS port is 443, and the alias after the # sign is a remark
@@ -23,7 +23,7 @@ let subconverter = 'url.v1.mk';// Clash subscription conversion backend, current
 let subconfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini.ini";// Subscription Profile
 let RproxyIP = 'false';
 
-let addressesapi = ['https://raw.githubusercontent.com/NiREvil/Trauma/main/cleanIPs.txt'];// you can use any addresses api's
+let addressesapi = [];// you can use any addresses api's like this ['https://raw.githubusercontent.com/NiREvil/Trauma/main/cleanIPs.txt'];
 let addressescsv = [];
 let DLS = 8;
 
